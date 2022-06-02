9b97e16eecd91a5addeb92201feec62c82b071aa
doms99 <dominikmandic@hotmail.com>
2022-06-02 22:04:59 +0200

    Configure light repo

---
+++ src/repository/light/lightRepositoryImpl.ts
@@ -0,0 +1,48 @@
+import { AWSError, AWSResponse, subscribe, UnsubscribeHolder } from "../../aws/aws_setup";
+import { SubscribeTopics } from "../../constants";
+import { LightObj } from "../../models/LightObj";
+import { LightListener, LightRepository } from "./lightRepository";
+
+type ListenerStorage = Record<string, LightListener>;
+
+export class LightRepositoryImpl implements LightRepository {
+  private listeners: ListenerStorage;
+  private nextID = 0;
+  private unsubscribeHolder: UnsubscribeHolder;
+
+  constructor() {
+    this.addListener = this.addListener.bind(this);
+    this.removeListener = this.removeListener.bind(this);
+    this.notifyListeners = this.notifyListeners.bind(this);
+
+    this.listeners = {};
+    this.unsubscribeHolder = subscribe(SubscribeTopics.LIGHT, {
+      next: (data: AWSResponse) => {
+        if(!data.value.state.reported) return
+
+        this.notifyListeners(data.value.state.reported as LightObj)
+      },
+      error: (error: AWSError) => console.log(error)
+    });
+  }
+
+  private notifyListeners(lightObj: LightObj) {
+    if(!lightObj.light) return
+
+    Object.values(this.listeners).forEach(listener => listener(lightObj))
+  }
+
+  addListener(listener: LightListener): string {
+    const id = `listener${this.nextID++}`;
+    this.listeners[id] = listener;
+
+    return id;
+  }
+
+  removeListener(id: string): boolean {
+    if(!this.listeners[id]) return false;
+
+    delete this.listeners[id];
+    return true;
+  }
+}
